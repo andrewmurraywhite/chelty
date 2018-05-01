b43bc942caa20ad0c3cf5c765fe8a2b3716613f4
andrewmurraywhite <andrewmurraywhite@gmail.com>
2018-05-01 23:24:51 +1200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express()
+
+app.get('/',(req, res) => res.send("Hello Andy!"))
+
+// http://localhost:3000/
+app.listen(3000, () => console.log("Am ristning"))
