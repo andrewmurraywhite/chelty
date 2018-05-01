6b43e4b3d187ac7086b18f05a2a04105184ca194
Andrew Murray White <Andyroo@awmbp.local>
2018-05-01 23:41:42 +1200

    test

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express()
 app.get('/',(req, res) => res.send("Hello Andy!"))
 
 // http://localhost:3000/
-app.listen(3000, () => console.log("Am ristning"))
+app.listen(3000, () => console.log("Am ristening"))
